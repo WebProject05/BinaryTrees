3d2f05efaa3486f7ccc30350794abb23d25004c3
Chopperla Santosh <chopperla2005@gmail.com>
2025-10-06 22:43:02 +0530

    add insertNode into BinaryHeap

--- BinaryHeap/BinaryHeap.py
+++ BinaryHeap/BinaryHeap.py
@@ -1,6 +1,6 @@
 class Heap:
     def __init__(self, size):
-        self.customList = [None] * (size + 1)  # Corrected list initialization
+        self.customList = [None] * (size + 1)
         self.heapSize = 0
         self.maxSize = size + 1
 
@@ -24,12 +24,51 @@ def levelOrderTraversal(rootNode):
             print(rootNode.customList[i], end=" ")
         print()
 
-newBinaryHeap = Heap(5)
 
-newBinaryHeap.customList[1] = 10
-newBinaryHeap.customList[2] = 20
-newBinaryHeap.customList[3] = 30
-newBinaryHeap.customList[4] = 40
-newBinaryHeap.heapSize = 4
+def heapifyTreeInsert(rootNode, index, heapType):
+    parentIndex = int(index / 2)
+    if index <= 1:
+        return
 
-levelOrderTraversal(newBinaryHeap)
+    if heapType == "Min":
+        if rootNode.customList[index] < rootNode.customList[parentIndex]:
+
+            rootNode.customList[index], rootNode.customList[parentIndex] = (
+                rootNode.customList[parentIndex],
+                rootNode.customList[index],
+            )
+        heapifyTreeInsert(rootNode, parentIndex, heapType)
+
+    elif heapType == "Max":
+        if rootNode.customList[index] > rootNode.customList[parentIndex]:
+            rootNode.customList[index], rootNode.customList[parentIndex] = (
+                rootNode.customList[parentIndex],
+                rootNode.customList[index],
+            )
+        heapifyTreeInsert(rootNode, parentIndex, heapType)
+
+
+def insertNode(rootNode, nodeValue, heapType):
+    if rootNode.heapSize + 1 == rootNode.maxSize:
+        return "The binary heap is full"
+    rootNode.customList[rootNode.heapSize + 1] = nodeValue
+    rootNode.heapSize = rootNode.heapSize + 1
+    heapifyTreeInsert(rootNode, rootNode.heapSize, heapType)
+
+
+# newBinaryHeap = Heap(5)
+
+# newBinaryHeap.customList[1] = 10
+# newBinaryHeap.customList[2] = 20
+# newBinaryHeap.customList[3] = 30
+# newBinaryHeap.customList[4] = 40
+# newBinaryHeap.heapSize = 4
+
+newHeap = Heap(5)
+
+insertNode(newHeap, 10, "Min")
+insertNode(newHeap, 32, "Min")
+insertNode(newHeap, 11, "Min")
+insertNode(newHeap, 1, "Min")
+
+levelOrderTraversal(newHeap)
