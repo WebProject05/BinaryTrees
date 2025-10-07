7fda50bcd7c0b8f1a646fafd5b14345265b4a395
Chopperla Santosh <chopperla2005@gmail.com>
2025-10-07 19:48:37 +0530

    add Trie DataStructure

---
+++ Trie/Trie.py
@@ -0,0 +1,30 @@
+class TrieNode:
+  def __init__(self):
+    self.children = {}
+    self.endOfString = False
+
+
+class Trie:
+  def __init__(self):
+    self.root = TrieNode()
+
+  def insertString(self, word):
+    current = self.root
+
+    for i in word:
+      ch = i
+      node = current.children.get(ch) # This is to check whether the node already exists in the tree or not
+      if (node == None):
+        node = TrieNode()
+        current.children.update({ch:node})
+      current = node
+    current.endOfString = True
+    print("Inserted!")
+
+
+newTrie = Trie()  #Initialiaztion of the trie class
+newTrie.insertString("San")
+newTrie.insertString("Sant")
+newTrie.insertString("Santo")
+newTrie.insertString("Santos")
+newTrie.insertString("Santosh")
