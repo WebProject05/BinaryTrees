ab88b9a12a4c7e6ec9f98d798eead4717b64c33d
Chopperla Santosh <chopperla2005@gmail.com>
2025-10-07 20:01:25 +0530

    add search string

--- Trie/Trie.py
+++ Trie/Trie.py
@@ -22,9 +22,29 @@ class Trie:
     print("Inserted!")
 
 
+
+  def searchString(self, word):
+    currentNode = self.root
+    for i in word:
+      node = currentNode.children.get(i)
+      if (node == None):
+        return False
+      
+      currentNode = node
+    if (currentNode.endOfString == True):
+      return True
+    else:
+      return False
+
+
+
 newTrie = Trie()  #Initialiaztion of the trie class
 newTrie.insertString("San")
 newTrie.insertString("Sant")
 newTrie.insertString("Santo")
 newTrie.insertString("Santos")
 newTrie.insertString("Santosh")
+
+
+print(newTrie.searchString("San"))
+print(newTrie.searchString("Sanosh"))
